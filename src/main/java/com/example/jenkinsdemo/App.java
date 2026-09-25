2dc64e7bff6f35e69df7efb8e74a7e93859b3491
Arya A <aryaanand053@gmail.com>
2026-09-25 09:34:50 +0530

    Update greet method call to handle null input

--- src/main/java/com/example/jenkinsdemo/App.java
+++ src/main/java/com/example/jenkinsdemo/App.java
@@ -3,7 +3,7 @@ package com.example.jenkinsdemo;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println(greet("Jenkins"));
+        System.out.println(greet(null));
     }
 
     public static String greet(String name) {
